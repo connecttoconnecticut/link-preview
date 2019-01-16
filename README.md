e60c509c9c37093e0a3b691ef666c939ad814e3f
connecttoconnecticut <boban96minic@gmail.com>
2019-01-16 15:01:41 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# link-preview
-node.js chat link preview
+# MongoChat
+
+Simple chat app that uses MongoDB and Socket.io
+
+### Version
+1.0.0
+
+## Install Dependencies
+```bash
+npm install 
+```
+
+## Run Server
+```bash
+npm start
+```
+
+## Run App
+Open index.html
---
+++ server.js
@@ -0,0 +1,95 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+const linkPreview = require ('./urlThumbnailRequest');
+
+
+var pattern = new RegExp('([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+');
+
+// Connect to mongo
+mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
+    if(err){
+        throw err;
+    }
+
+    console.log('MongoDB connected...');
+
+    // Connect to Socket.io
+    client.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+        // Create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            // Check for name and message
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
+            } else {
+
+                // link preview integration
+                if(urlValidation(message)){
+                    var urlData;
+                    //var urlData = linkPreview.startDiscover(pattern.exec(message)[0]);
+                    linkPreview.startDiscover(pattern.exec(message)[0],function(results){
+                        urlData = results;
+                        console.log('This is url data: ',urlData);
+                        chat.insert({name: name, message: message, link : urlData.link, title: urlData.title, img: urlData.img}, function(){
+                            client.emit('output', [data]);
+    
+                            // Send status object
+                            sendStatus({
+                                message: 'Message sent',
+                                clear: true
+                            });
+                        });
+                    })
+                    
+
+                }else{
+                    // Insert SIMPLE message
+                    chat.insert({name: name, message: message}, function(){
+                        client.emit('output', [data]);
+
+                        // Send status object
+                        sendStatus({
+                            message: 'Message sent',
+                            clear: true
+                        });
+                    });
+                }
+                
+            }
+        });
+
+        // Handle clear
+        socket.on('clear', function(data){
+            // Remove all chats from collection
+            chat.remove({}, function(){
+                // Emit cleared
+                socket.emit('cleared');
+            });
+        });
+    });
+});
+
+var urlValidation = (url) => {
+    return pattern.test(url);
+}
